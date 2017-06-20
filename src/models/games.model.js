bbe083f033ae33196aec292b63862ebd356e6f86
ChrisH81 <chrishurst1981@icloud.com>
2017-06-20 11:48:26 +0200

    Created API and Schema

---
+++ src/models/games.model.js
@@ -0,0 +1,36 @@
+// games-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const gamesSchema = new Schema({
+    pads: [padSchema],
+    started: { type: Boolean, required: true, 'default': false },
+    demo: { type: Boolean, required true 'default': false},
+    players: [playerSchema],
+    turn: { type: Number, required: true, 'default': 0 },
+    winner: { type: Number, required: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    userId: { type: Schema.Types.ObjectId, ref: 'user' }
+  });
+
+  const playerSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'user' },
+    name: { type: String, required: true },
+    score: { type: Number, required: true, 'default': 0 },
+    lives: { type: Number, required: true, 'default': 3 }
+  });
+
+  const padSchema = new Schema({
+    color: { type: String, required: true },
+    active: { type: boolean, required: true, default: false},
+
+  })
+
+  const gamesModel = mongoose.model('games', GamesSchema)
+
+  module.exports = GamesModel;
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+const games = require('./games/games.service.js');
+module.exports = function () {
+  const app = this; // eslint-disable-line no-unused-vars
+  app.configure(games);
+};
